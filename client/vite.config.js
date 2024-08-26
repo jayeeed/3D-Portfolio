4e779b20c2e42b39d50d2434fe556efe99217ee7
jayeeed <jayedbinjahangir@gmail.com>
2024-08-27 05:56:30 +0600

    minify #2

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,17 +3,41 @@ import react from "@vitejs/plugin-react";
 import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 import replace from "@rollup/plugin-replace";
 import { visualizer } from "rollup-plugin-visualizer";
+import packageJSON from "./package.json" assert { type: "json" };
+import pluginTerser from "@rollup/plugin-terser";
 
+// https://vitejs.dev/config/
 export default ({ mode }) => {
   // Load app-level env vars to node-level env vars.
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  // https://vitejs.dev/config/
-  return defineConfig({
+  return {
+    input: "bundle.js",
+    output: [
+      {
+        file: "dist/" + packageJSON.name + ".cjs.js",
+        format: "cjs",
+      },
+      {
+        file: "dist/" + packageJSON.name + ".es.js",
+        format: "es",
+      },
+      {
+        name: "myObject",
+        file: "dist/" + packageJSON.name + ".iife.js",
+        format: "iife",
+      },
+      {
+        name: "myObject",
+        file: "dist/" + packageJSON.name + ".umd.js",
+        format: "umd",
+      },
+    ],
     plugins: [
       react(),
       chunkSplitPlugin(),
       visualizer(),
+      pluginTerser(),
       replace({
         "eval(": "((x)=>x)(", // Replacing eval with a no-op or any other logic
         preventAssignment: true,
@@ -23,11 +47,11 @@ export default ({ mode }) => {
       port: Number(process.env.VITE_PORT) || 3000,
     },
     build: {
-      minify: "esbuild",
-      sourcemap: false,
-      outDir: "dist",
-      assetsDir: "assets",
-      chunkSizeWarningLimit: 1000,
+      minify: false,
+      terserOptions: {
+        compress: false,
+        mangle: false,
+      },
     },
     rollupOptions: {
       output: {
@@ -37,5 +61,5 @@ export default ({ mode }) => {
         },
       },
     },
-  });
+  };
 };
