789c0c7cbfb748e56209767d529c91c9473475d1
jayeeed <jayedbinjahangir@gmail.com>
2024-08-28 02:40:16 +0600

    mig next

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
-
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
